49b0a8d1d089f827d15a848f94a245709613e5c5
Dan Griffin <dangriffin13@gmail.com>
2017-07-01 00:08:13 -0400

    dynamic url

--- site_dir/mysite/views.py
+++ site_dir/mysite/views.py
@@ -11,3 +11,12 @@ def current_datetime(request):
 	now = datetime.datetime.now()
 	html = "<html><body>It's currently %s </body></html>" % now
 	return HttpResponse(html)
+
+def hours_ahead(request, offset):
+	try:
+		offset = int(offset)
+	except ValueError:
+		raise Http404()
+	dt = datetime.datetime.now() + datetime.timedelta(hours=offset)
+	html = "<html><body> In %s hours, it will be %s. </body></html>" % (offset, dt)
+	return HttpResponse(html)
