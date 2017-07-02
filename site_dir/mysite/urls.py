97393a0a49f8b7d73b98b06588c84ec2224cd355
Dan Griffin <dangriffin13@gmail.com>
2017-07-02 17:33:44 -0400

    template tag changes

--- site_dir/mysite/urls.py
+++ site_dir/mysite/urls.py
@@ -22,5 +22,6 @@ urlpatterns = [
     url(r'^$', null),
     url(r'^hello/$', hello),
     url(r'^time/$', current_datetime),
-    url(r'^time/plus/(\d{1,2})/$', hours_ahead)
+    url(r'^time/plus/(\d{1,2})/$', hours_ahead),
+    
 ]
