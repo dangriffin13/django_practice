bf2b091a5341a16009803ffdd8f5d6eb07edf15e
Dan Griffin <dangriffin13@gmail.com>
2017-07-13 02:37:13 -0400

    admin page

--- site_dir/books/admin.py
+++ site_dir/books/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
 from .models import Publisher, Author, Book
 
+class AuthorAdmin(admin.ModelAdmin):
+	list_display = ('first_name','last_name','email')
+	search_fields = ('first_name', 'last_name')
+	list_filter = ('last_name',)
+	ordering = ('last_name','first_name')
+	#fields = ('last_name','first_name', 'email')
+
 admin.site.register(Publisher)
-admin.site.register(Author)
+admin.site.register(Author, AuthorAdmin)
 admin.site.register(Book)
