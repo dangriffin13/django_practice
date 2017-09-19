f671d7bfd83a9767f72fc51e0d0c2b7398a93520
Dan Griffin <dangriffin13@gmail.com>
2017-09-18 23:52:04 -0400

    hook into models

--- site_dir/books/views.py
+++ site_dir/books/views.py
@@ -6,8 +6,9 @@ def search_form(request):
 	return render(request, 'books/search_form.html')
 
 def search(request):
-	if 'q' in request.GET:
-		message = 'You searched for: %r' % request.GET['q']
+	if 'q' in request.GET and request.GET['q']:
+		q = request.GET['q']
+		books = Book.objects.filter(title__icontains=q)
+		return render(request, 'books/search_results.html', {'books': books, 'query': q})
 	else:
-		message = 'You submitted an empty form.'
-	return HttpResponse(message)
+		return HttpResponse('Please submit a search term.')
