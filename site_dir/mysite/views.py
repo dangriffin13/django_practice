6db83117d24e11e68853cca9af7eeb59226360a9
Dan Griffin <dangriffin13@gmail.com>
2017-10-04 23:12:58 -0400

    forms view

--- site_dir/mysite/views.py
+++ site_dir/mysite/views.py
@@ -1,7 +1,9 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.template.loader import get_template
 from django.shortcuts import render
 from django.template import Template, Context
+from django.core.mail import send_mail, get_connection
+from mysite.forms import ContactForm
 import datetime
 
 def hello(request):
@@ -48,3 +50,21 @@ def display_meta(request):
 	return HttpResponse('<table>%s</table>' % '\n'.join(html))
 
 
+def contact(request):
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            con = get_connection('django.core.mail.backends.console.EmailBackend')
+            send_mail(
+                cd['subject'],
+                cd['message'],
+                cd.get('email', 'noreply@example.com'),
+                ['siteowner@example.com'],
+                connection=con
+            )
+            return HttpResponseRedirect('/contact/thanks/')
+    else:
+        form = ContactForm()
+
+    return render(request, 'contact_form.html', {'form': form})
